69fa9ddbb8830588a92c46f67b75b0a357b09a98
fkrein1 <felipekrein@gmail.com>
2022-05-27 19:03:40 -0300

    add context

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,20 +1,12 @@
 import { useContext } from 'react';
-import { useHistory } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import ilustrationImg from '../assets/illustration.svg';
 import logoImg from '../assets/logo.svg';
-import googleIconImg from '../assets/google-icon.svg'
 import '../styles/auth.scss'
 import { Button } from '../components/Buttons';
 
-export function Home() {
-  const history = useHistory();
 
-  function handleCreateRoom() {
-    history.push('/room/new');
-  }
-
-
-
+export function NewRoom() {
   return (
     <div id="page-auth">
       <aside>
@@ -25,20 +17,17 @@ export function Home() {
       <main>
         <div className="main-content">
           <img src={logoImg} alt="Letmeask" />
-          <button className="create-room" onClick={handleCreateRoom}>
-            <img src={googleIconImg} alt="" />
-            Crie sua sala com o Google
-          </button>
-          <div className="separator">ou entre em uma sala</div>
+          <h2>Criar uma nova sala</h2>
           <form>
             <input 
               type="text"
-              placeholder="Digite o código da sala"
+              placeholder="Nome da sala"
             />
             <Button type="submit">
-              Entrar na sala
+              Criar sala
             </Button>
           </form>
+          <p>Quer entrar em uma sala existente? <Link to="/">clique aqui</Link></p>
         </div>
       </main> 
     </div>
